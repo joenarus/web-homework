9a780495d7f2ac77ff412c57274991461672bb34
joenarus <joe.narus2@gmail.com>
2020-12-27 12:55:27 -0700

    successfully adding transactions from form

--- webapp/src/queries/transaction-queries.js
+++ webapp/src/queries/transaction-queries.js
@@ -19,3 +19,26 @@ export const GET_TRANSACTIONS = gql`
         }
     }
 `
+export const ADD_TRANSACTION = gql`
+    
+        mutation(
+            $amount: Int!, 
+            $debit: Boolean!, 
+            $credit: Boolean!, 
+            $description: String!, 
+            $merchant: ID!, 
+            $user: ID!
+        ){
+            createTransaction(
+                amount: $amount, 
+                description: $description, 
+                credit: $credit, 
+                debit: $debit,
+                merchantId: $merchant, 
+                userId: $user
+            ){
+                id
+            }
+        }   
+    
+`
--- webapp/src/transactions/add-transactions.js
+++ webapp/src/transactions/add-transactions.js
@@ -1,8 +1,9 @@
 
 import React, { useState, useEffect, Fragment } from 'react'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 import { GET_MERCHANTS } from '../queries/merchant-queries'
 import { GET_USERS } from '../queries/user-queries'
+import { ADD_TRANSACTION } from '../queries/transaction-queries'
 // import { GET_USERS } from '../queries/user-queries'
 
 // const useUserState = () => {
@@ -22,6 +23,8 @@ export function AddTransaction () {
   const [users, setUsers] = useState([])
   const merchantData = useQuery(GET_MERCHANTS)
   const userData = useQuery(GET_USERS)
+  const [createTransaction] = useMutation(ADD_TRANSACTION)
+
   useEffect(() => {
     if (merchantData.data && merchantData.data.merchants) {
       setMerchants(merchantData.data.merchants)
@@ -30,23 +33,22 @@ export function AddTransaction () {
     }
   }, [merchantData.data, userData.data])
 
-  const [currentTransaction, setCurrentTransaction] = useState({ amount: null, user: null, merchant: null, description: null, credit: false, debit: false })
+  const [currentTransaction, setCurrentTransaction] = useState({ amount: '', user: '', merchant: '', description: '', credit: false, debit: false })
+
   const handleInputChange = event => {
     const target = event.target
-    // eslint-disable-next-line no-debugger
-    debugger
-    const value = target.type === 'checkbox' ? target.checked : target.value
-
-    const name = target.name
-
+    const value = target.type === 'radio' ? target.checked : target.name === 'amount' ? parseInt(target.value) : target.value
+    const name = target.type === 'radio' ? target.value : target.name
     const transaction = { ...currentTransaction, [name]: value }
     setCurrentTransaction(transaction)
   }
 
   const handleSubmit = event => {
     event.preventDefault()
+    createTransaction({ variables: currentTransaction })
+    console.log('here')
     // Query will go here
-    setCurrentTransaction({ amount: '', user: '', merchant: '', description: '', credit: false, debit: false })
+    setCurrentTransaction({ amount: null, user: null, merchant: null, description: null, credit: false, debit: false })
   }
 
   return (
@@ -55,24 +57,34 @@ export function AddTransaction () {
 
         <h3>Add Transaction</h3>
         <label>
+      Debit
+          <input name='debit-or-credit' onChange={handleInputChange} required type='radio' value='debit' />
+        </label>
+        <label>
+      Credit
+          <input name='debit-or-credit' onChange={handleInputChange} type='radio' value='credit' />
+        </label>
+        <label>
       Amount
-          <input name='amount' onChange={handleInputChange} type='text' />
+          <input name='amount' onChange={handleInputChange} type='number' value={currentTransaction.amount} />
         </label>
         <label>
       User
-          <select name='user' onBlur={handleInputChange} >
+          <select name='user' onBlur={handleInputChange} onChange={handleInputChange} required value={currentTransaction.user}>
+            <option value={null}>None</option>
             {users.map((user) => <option key={user.id} value={user.id}>{user.firstName + ' ' + user.lastName}</option>)}
           </select>
         </label>
         <label>
       Merchant
-          <select name='merchant' onBlur={handleInputChange}>
+          <select name='merchant' onBlur={handleInputChange} onChange={handleInputChange} required value={currentTransaction.merchant}>
+            <option value={null}>None</option>
             {merchants.map((merchant) => <option key={merchant.id} value={merchant.id}>{merchant.name}</option>)}
           </select>
         </label>
         <label>
       Description
-          <input name='description' onChange={handleInputChange} type='text' />
+          <input name='description' onChange={handleInputChange} type='text' value={currentTransaction.description} />
         </label>
         <input type='submit' value='Save' />
 
